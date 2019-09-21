dfb4616c0716d05dbcc9acd263460e6a177f853f
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2019-09-21 15:32:49 +1200

    WIP

--- script/generate-docker-tag.sh
+++ script/generate-docker-tag.sh
@@ -13,6 +13,7 @@ echo "********************************************"
 echo ${build_source_branch}
 echo "********************************************"
 
+
 if [[ "$build_source_branch" == refs/pull* ]]; then
     # building repository name triggered by Pull Request
     build_source_branch="${build_source_branch//refs\/pull\//}"
